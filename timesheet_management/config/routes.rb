8980b929b43bdf578e10a0d4a5bf16089f9477f6
krishna <krishna@ideology.com>
2015-04-15 12:22:47 +0530

    add devise

--- timesheet_management/config/routes.rb
+++ timesheet_management/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :roles
+
+  devise_for :users
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -41,7 +44,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+root :to => 'home#index'
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
--- timesheet_management/db/seeds.rb
+++ timesheet_management/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email:"venkat.ideology@gmail.com", password: "password", password_confirmation: "password", first_name: "Raj", last_name: "Kiran")
+User.create(email:"dinesh.ideology@gmail.com", password: "password", password_confirmation: "password", first_name: "Dinesh", last_name: "G")
+User.create(email:"krishna.ideology@gmail.com", password: "password", password_confirmation: "password", first_name: "Krishna", last_name: "Varma")
